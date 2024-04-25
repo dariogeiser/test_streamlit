4387d47d4ae641c3ed86ce2e34432e2b5837ffe7
TAAGEDA3 <dario.geiser@swisscom.com>
2024-04-25 10:25:53 +0200

    first commit

---
+++ uber_pickups.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+st.markdown('[Follow the tutorial here](https://docs.streamlit.io/get-started/tutorials/create-an-app)')
+
